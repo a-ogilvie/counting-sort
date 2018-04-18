275773deb554ee31657c9b9641b317b4a7047d83
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-18 16:03:29 +0900

    Write basic test for CountingSort

--- test/_sort.js
+++ test/_sort.js
@@ -5,7 +5,15 @@ describe("Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
+
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
+  });
+
+  it("should sort an array from lowest value to highest value", () => {
+    const unsorted = new Sort([5, 6, 4, 3, 1, 2, 2, 7]);
+    const sorted = [1, 2, 2, 3, 4, 5, 6, 7];
+    unsorted.sort();
+    expect(unsorted.array).to.deep.equal(sorted);
   });
 });
