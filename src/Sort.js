c9a00f09ca437f18e71328af074e1e0064e6f22c
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-18 18:10:09 +0900

    Simplify sort to only sort the array and return the count object

--- src/Sort.js
+++ src/Sort.js
@@ -9,56 +9,32 @@ class CountingSort {
   }
 
   /**
-   * Sort this.array using the counting sort algorithm and visualise.
-   * @param {HTMLElement} arrayElem
-   * @param {HTMLElement} objectElem
+   * Sort this.array using the counting sort algorithm and return the counts.
+   * @returns {Object}
    */
-  sort(arrayElem, objectElem) {
-    const arrayValues = { a: "foo", b: "bar" };
-    Object.defineProperty(arrayValues, "toString", {
-      value: function stringObject() {
-        return Object.entries(this);
-      },
-      enumerable: false,
-    });
-    console.log(arrayValues.toString());
+  sort() {
+    const counts = {};
+
     let maxValue = 0;
     this.array.forEach((value) => {
       if (maxValue < value) maxValue = value;
-      if (!arrayValues.hasOwnProperty(value)) {
-        arrayValues[value] = 1;
+      if (!counts.hasOwnProperty(value)) {
+        counts[value] = 1;
       } else {
-        arrayValues[value]++;
+        counts[value]++;
       }
     });
 
     const result = [];
     for (let i = 0; i <= maxValue; i++) {
-      while (arrayValues[i] && arrayValues[i] > 0) {
+      while (counts[i] && counts[i] > 0) {
         result.push(i);
-        arrayValues[i]--;
+        counts[i]--;
       }
     }
     this.array = result;
-  }
-
-  /**
-   * Generate an HTML element from the provided item.
-   * @param {*} item
-   * @returns {HTMLElement}
-   */
-  generateNewElem(item) {
-    const container = document.createElement("div");
-    const textNode = document.createTextNode();
-    container.appendChild(textNode);
-    return container;
-  }
-
-  createCheesyTitle(slogan) {
-    const container = document.createElement("h1");
-    const textNode = document.createTextNode(slogan);
-    container.appendChild(textNode);
-    return container;
+    console.log(result);
+    return counts;
   }
 }
 
