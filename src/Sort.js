663cd5172da59dfd34cf10bb62ddc1c37595c702
a-ogilvie <alexander.ogilvie@icloud.com>
2018-04-18 16:34:11 +0900

    Remove unnecessary returnValue() from CountingSort class

--- src/Sort.js
+++ src/Sort.js
@@ -1,19 +1,16 @@
-/** Class which can sort an array using the counting sort algorithm. */
+/** Class which can sort an array of positive integers using the counting sort algorithm. */
 class CountingSort {
   /**
    * Initialise with an array
    * @param {Array} array
    */
-  constructor(array) {
+  constructor(array = []) {
     this.array = array;
   }
 
   /** Sort this.array using the counting sort algorithm. */
   sort() {
-    // Generate an empty object
     const arrayValues = {};
-    // Add 1 to each value in the new object with key equal to the index of the numbers
-    // And find the largest value in the array
     let maxValue = 0;
     this.array.forEach((value) => {
       if (maxValue < value) maxValue = value;
@@ -23,7 +20,7 @@ class CountingSort {
         arrayValues[value]++;
       }
     });
-    // Refill the original array from smallest to largest value
+
     const result = [];
     for (let i = 0; i <= maxValue; i++) {
       while (arrayValues[i] && arrayValues[i] > 0) {
@@ -32,15 +29,6 @@ class CountingSort {
       }
     }
     this.array = result;
-  }
-
-  /**
-   * Immediately returns the value with which it is provided.
-   * @param {*} value
-   * @returns {*} value
-   */
-  returnValue(value) {
-    return value;
   }
 }
 
